ae55bea92d04111258249790c9260bdf0608549f
gunwookong <37990722+gunwookong@users.noreply.github.com>
2018-08-30 16:16:51 +0900

    Modified
    180830

--- PPP_Generate.py
+++ PPP_Generate.py
@@ -1,8 +1,4 @@
-# PPP_generate:density, scope 입력하면 좌표 생성
-# BSAntenna: Base Station 안테나
-# UEAntenna: User Equipment 안테나
 import numpy as np
-# import math
 
 
 def ppp_generate(ppp_density, low_x, high_x, low_y, high_y):
@@ -14,6 +10,3 @@ def ppp_generate(ppp_density, low_x, high_x, low_y, high_y):
 
     node_coordinate = [tmp_x, tmp_y]
     return node_coordinate
-
-# a= ppp_generate(2*math.pow(10, -4), 0,1000,0,1000)
-# print(a[0])
--- User_Path.py
+++ User_Path.py
@@ -1,12 +1,9 @@
-# UEAntenna
-
 # 원점(0,0) 부터 출발하는 경로 함수 설정
-# import math
 import numpy as np
 
 
 def user_path(low_x, high_x):
-    path_x = np.arange(low_x, high_x, 20)
+    path_x = np.arange(low_x, high_x, 25)
     path_y = 1/1000 * path_x * path_x
     node_coordinate = [path_x, path_y]
 
